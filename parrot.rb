32cb7de1888ca8462e01e6148b72caa2db7bc50b
organized-trainer-0561 <jared.epps322@gmail.com>
2020-03-07 01:37:35 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,21 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
+An error occurred while loading spec_helper.
+Failure/Error: require_relative '../parrot.rb'
+
+SyntaxError:
+  /home/organized-trainer-0561/parrot-ruby-onl01-seng-pt-030220/parrot.rb:1: syntax error, unexpected tSTRING_BEG, expecting ')'
+  def parrot("Pretty bird!")
+             ^
+  /home/organized-trainer-0561/parrot-ruby-onl01-seng-pt-030220/parrot.rb:1: syntax error, unexpected ')', expecting end-of-input
+  def parrot("Pretty bird!")
+                           ^
+# ./spec/spec_helper.rb:5:in `require_relative'
+# ./spec/spec_helper.rb:5:in `<top (required)>'
+No examples found.
+No examples found.
+
+
+Finished in 0.00004 seconds (files took 0.09015 seconds to load)
+0 examples, 0 failures, 1 error occurred outside of examples
+
+Finished in 0.00004 seconds (files took 0.09015 seconds to load)
+0 examples, 0 failures, 1 error occurred outside of examples
